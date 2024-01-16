085b9e70dcb0556356fa0ef55c0c07eacf5d6f71
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-01-16 19:47:23 +0200

    add scan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Quagga from "quagga";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [barcode, setBarcode] = useState("");
+  const [error, setError] = useState(null);
+
+  const cameraScannerRef = useRef();
+
+  useEffect(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#scanner-container"),
+          constraints: {
+            width: 640,
+            height: 480,
+            facingMode: "environment", // вибір тилової камери
+          },
+        },
+        decoder: {
+          readers: ["ean_reader", "upc_reader"],
+        },
+      },
+      function (err) {
+        if (err) {
+          // console.error("Помилка ініціалізації Quagga: ", err);
+          setError(err);
+          return;
+        }
+        Quagga.start();
+      }
+    );
+
+    Quagga.onDetected((result) => {
+      // Обробка знайденого штрихкоду
+      // console.log("Знайдено штрихкод:", result.codeResult.code);
+      setBarcode(result.codeResult.code);
+    });
+
+    return () => {
+      Quagga.stop();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="barcode">{barcode}</p>
+      <div
+        ref={cameraScannerRef}
+        id="scanner-container"
+        className="scanner-container cameraScanner"
+      >
+        {/* Відображення відеопотоку з камери */}
+      </div>
+      {error && <p className="errorMessage">{JSON.stringify(error)}</p>}
     </div>
   );
 }
